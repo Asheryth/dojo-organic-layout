d8050d5569fd0b2d10f5cad94738277b530196c3
Loïc Sifferlen <lsifferlen@betomorrow.com>
2019-05-23 14:36:50 +0200

    Add data format infos

--- updater.js
+++ updater.js
@@ -1,4 +1,19 @@
 
+/**
+ *  data = {
+ *    entreprises: [{     | Noeuds du graph
+ *      x: int,           | Position en X
+ *      y: int,           | Position en Y
+ *      size: int         | Taille (taille de l'entreprise)
+ *    }],
+ *    transactions: [{    | Liens du graph
+ *      e1: int,          | Index du premier noeud
+ *      e2: int,          | Index du deuxième noeud
+ *      count: int        | Taille (nombre de transactions entre deux entreprises)
+ *    }] 
+ *  }
+ */
+
 function init(data) {
   
 }
